407d2d911445c9507a2e57b1ecbf8b9fc7d35bcc
hashem98 <hashemsmadi98@gmail.com>
2022-04-05 21:52:50 +0300

    this app  is Done

--- src/main/java/com/example/CodeFellowship/Controller/profileController.java
+++ src/main/java/com/example/CodeFellowship/Controller/profileController.java
@@ -1,13 +1,11 @@
 package com.example.CodeFellowship.Controller;
 
-
 import com.example.CodeFellowship.Model.ApplicationUser;
 import com.example.CodeFellowship.Repositories.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import java.security.Principal;
 
 @Controller
--- src/main/java/com/example/CodeFellowship/Controller/signUpController.java
+++ src/main/java/com/example/CodeFellowship/Controller/signUpController.java
@@ -1,56 +1,51 @@
 package com.example.CodeFellowship.Controller;
 
+
 import com.example.CodeFellowship.Model.ApplicationUser;
 import com.example.CodeFellowship.Repositories.ApplicationUserRepository;
-
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.web.bind.annotation.*;
-
 import org.springframework.web.servlet.view.RedirectView;
-
 import java.sql.Date;
-
+import java.util.ArrayList;
 
 @Controller
 public class signUpController {
 
 
-    @RequestMapping("/signup")
-    public String signupPage(){
-        return "SignUp";
-    }
-
     @Autowired
     ApplicationUserRepository applicationUserRepository;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @GetMapping("/signup")
+    public String signupPage(){
+        return "signup";
+    }
+
 
     @PostMapping("/signup")
-    public RedirectView createUSer(@RequestParam String username,
-                                   @RequestParam String password,
-                                   @RequestParam String firstName,
-                                   @RequestParam String lastName,
-                                   @RequestParam Date date,
-                                   @RequestParam String bio){
-        password = passwordEncoder.encode(password);
+    public RedirectView createUSer( String userName,
+                                    String password,
+                                    String firstName,
+                                    String lastName,
+                                    Date dateOfBirth,
+                                    String bio){
 
-        ApplicationUser user = new ApplicationUser(username, password, firstName, lastName, date, bio);
-        applicationUserRepository.save(user);
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-                user, null, user.getAuthorities()
-        );
 
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-        return new RedirectView("/userDetail");
+        ApplicationUser newUser = new ApplicationUser(userName, bCryptPasswordEncoder.encode(password), firstName, lastName, dateOfBirth, bio);
+        applicationUserRepository.save(newUser);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(newUser, null, new ArrayList<>());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        return new RedirectView("/profile");
     }
+
 }
